5071a675f69ec13e68c974e21eba433b12e443a5
Uyen Nguyen <uyenlocal@FVFYJ2T7HV2H.local>
2022-06-22 19:16:34 +0700

    Refactor code Lab 601

--- src/lab_06/Lab_601.java
+++ src/lab_06/Lab_601.java
@@ -4,16 +4,20 @@ public class Lab_601 {
 
     public static void main(String[] args) {
         String time = "2hrs and 5 minutes";
+        String[] hourAndMinute = time.split(" and ");
+        for (String each: hourAndMinute) {
+            System.out.println(each);
+        }
         System.out.print("Minutes total: ");
-        System.out.println(caculateMinutes(getMinutes(time), getHours(time)));
+        System.out.println(caculateMinutes(getMinutes(hourAndMinute), getHours(hourAndMinute)));
     }
 
-    public static int getMinutes(String time){
-        return Integer.parseInt(time.substring(9, time.indexOf(" minutes")));
+    public static int getMinutes(String[] array) {
+        return Integer.parseInt(array[1].substring(0, array[1].indexOf(" minutes")));
     }
 
-    public static int getHours(String time) {
-        return Integer.parseInt(time.substring(0, time.indexOf("hrs")));
+    public static int getHours(String[] array) {
+        return Integer.parseInt(array[0].substring(0, array[0].indexOf("hrs")));
     }
 
     public static int caculateMinutes(int minutes, int hours){
